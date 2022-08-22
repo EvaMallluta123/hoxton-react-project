0bdb58121f674c22b15d4ac0df0b9aa9caec4a51
Eva Malluta <eva.malluta14@gmail.com>
2022-08-22 13:40:19 +0200

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,20 @@
 import { useState } from "react";
 import "./App.css";
-import { Link } from "react-router-dom";
+import { Link, Routes , Route} from "react-router-dom";
 import { Header } from "./Components/Header";
+import { Products } from "./Components/Products";
 
 function App() {
   return (
     <div className="App">
    <Header />
-   <li className="product">
-   {/* <Link to={`/products/${product.id}`}> */}
-
-
-
-   <article className="product-item">
-  <img src="https://www.neptun.al/2022/08/11/TL3686.JPG?width=185" alt="" />
-  <h4>SAMSUNG GALAXY Z Fold4 5G Green</h4>
-</article>
-
-<article className="product-item">
-  <img src="https://www.neptun.al/2022/08/11/TL3686.JPG?width=185" alt="" />
-  <h4>SAMSUNG GALAXY Z Fold4 5G Green</h4>
-</article>
-
-
-<article className="product-item">
-  <img src="https://www.neptun.al/2022/08/11/TL3686.JPG?width=185" alt="" />
-  <h4>SAMSUNG GALAXY Z Fold4 5G Green</h4>
-</article>
-{/* </Link> */}
-</li>
+   <>
+   <main>
+   <Routes>
+   <Route path='/products' element={<Products />} />
+   </Routes>
+   </main>
+   </>
    </div>
   )
 }
