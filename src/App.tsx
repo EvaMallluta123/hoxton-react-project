299b9d1cd86ca16b6398d7c5653700850adf720a
Eva Malluta <eva.malluta14@gmail.com>
2022-08-22 11:36:47 +0200

    done with the style of the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
 import { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Link } from "react-router-dom";
+import { Header } from "./Components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="header">
-        <h1 className="header-logo">e-ShopiAl</h1>
-        <nav className="header-nav">
-          <ul>
-            <li>
-              <Link to="products"> Products</Link>
-            </li>
-            <li>
-              <Link to="categories"> Categories</Link>
-            </li>
-            <li>
-              <Link to="basket">Basket</Link>
-            </li>
-            <li>
-              <Link to="sing-in"> Sing-In</Link>
-            </li>
-            <li>
-              <button>Sign Out</button>
-            </li>
-          </ul>
-        </nav>
-      </header>
+   <Header />
+
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Components/Header.tsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+
+export function Header(){
+    return(
+        <header className="header">
+        <h2 className="header-logo">e-ShopiAl</h2>
+        <nav className="header-nav">
+          <ul>
+            <li >
+              <Link to="products" className="header-nav-li"> Products</Link>
+            </li>
+            <li>
+              <Link to="categories" className="header-nav-li"> Categories</Link>
+            </li>
+            <li>
+              <Link to="basket" className="header-nav-li">Basket</Link>
+            </li>
+            <li>
+              <Link to="sing-in" className="header-nav-li"> Sing-In</Link>
+            </li>
+            <li>
+              <button>Sign Out</button>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    )
+}
