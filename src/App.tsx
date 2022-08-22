f3231863f867b99db14a4bb74c96488c01efc070
Eva Malluta <eva.malluta14@gmail.com>
2022-08-22 11:09:06 +0200

    working on header

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,35 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+      <header className="header">
+        <h1 className="header-logo">e-ShopiAl</h1>
+        <nav className="header-nav">
+          <ul>
+            <li>
+              <Link to="products"> Products</Link>
+            </li>
+            <li>
+              <Link to="categories"> Categories</Link>
+            </li>
+            <li>
+              <Link to="basket">Basket</Link>
+            </li>
+            <li>
+              <Link to="sing-in"> Sing-In</Link>
+            </li>
+            <li>
+              <button>Sign Out</button>
+            </li>
+          </ul>
+        </nav>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
