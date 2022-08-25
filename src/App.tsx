1a5e6b6834efb2d9b6f1d8aa6da7121247408296
Eva Malluta <eva.malluta14@gmail.com>
2022-08-25 10:17:35 +0200

    working on basket

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <>
+      {/* <>
         <main>
           <Routes>
             <Route path="/products" element={<Products />} />
@@ -22,8 +22,33 @@ function App() {
             <Route path="/sing-in" />
           </Routes>
         </main>
-      </>
+      </> */}
   
+  <section className="basket">
+  <h2>Welcome to your Basket</h2>
+  <ul>
+  <li >
+      <article className='basket_item'>
+        <img src="src/image/lodra1.webp" alt="jfkjasfdkdsajfkjk" width='90' />
+        <p>sjdlkjdkAJFKDSJKF</p>
+        <p>
+          Qty:
+          <select>
+            <option value='0'>0</option>
+            <option value='1'>1</option>
+            <option value='2'>2</option>
+            <option value='3'>3</option>
+            <option value='4'>4</option>
+          </select>
+        </p>
+        <p>Item total: £1234</p>
+      </article>
+    </li>
+  </ul>
+  <h3>Your total: £123</h3>
+
+
+  </section>
     </div>
   );
 }
